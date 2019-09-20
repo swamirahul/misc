a2a5487d480283dcd05a22155094cda7bff9ec0a
Rahul Swami <48069099+swamirahul@users.noreply.github.com>
2019-09-20 18:56:48 -0400

    Update Print_diagonal_elements.py

--- Print_diagonal_elements.py
+++ Print_diagonal_elements.py
@@ -1,4 +1,57 @@
+Give a N*N square matrix, return an array of its anti-diagonals. Look at the example for more details.
+
+For Example:
+If the matrix is    
+
+1 2 3
+4 5 6
+7 8 9
+The output should Return the following :
+
+[ 
+  [1],
+  [2, 4],
+  [3, 5, 7],
+  [6, 8],
+  [9]
+]
+i.e print the elements of array diagonally.
+
+Note: The input array given is in single line and you have to output the answer in one line only.
+
+ 
+
+Input:
+
+The first line contains an integer T, depicting total number of test cases. 
+Then following T lines contains an integer N depicting the size of square matrix and next line followed by the value of array.
+
+
+Output:
+
+Print the elements of matrix diagonally in separate line.
+
+
+Constraints:
+
+1 ≤ T ≤ 30
+1 ≤ N ≤ 20
+0 ≤ A[i][j] ≤ 9
+
+
+Example:
+
+Input:
+2
+2
+1 2 3 4
+3
+1 2 3 4 5 6 7 8 9
+Output:
+1 2 3 4
+1 2 4 3 5 7 6 8 9
 #code
+
 def sumarrary(somelist,tempsum):
     sumlist=[]
     for x in somelist:
